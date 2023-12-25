e8277ceb9db9673e67952457e7020c3bba8c645d
Salmamaazoum <134633040+Salmamaazoum@users.noreply.github.com>
2023-12-25 18:48:15 +0100

    Update test_localisations.py

--- tests/test_localisations.py
+++ tests/test_localisations.py
@@ -1,1 +1,13 @@
+from stats.localisation import localisations
+from pytest import *
 
+
+def test_localisations():
+    # Given
+    filename = 'Fusillade.csv'
+    # Then
+    assert localisations(filename) == (738, 781, 759)
+    # Given
+    filename1 = 'Viol.csv'
+    # Then
+    assert len(localisations(filename1)) == 3
